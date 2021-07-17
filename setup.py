c6b2cad1fcedd098f0d64ae2cf2a9cb7d0f647b4
Yosef Alemneh <yosefworku67@gmail.com>
2021-07-17 20:40:54 +0300

    modified requirements.txt and setup.py files

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ requirements = ['pandas>=1.1.0', 'numpy>=1.19.0', ]
 test_requirements = ['pytest>=3', ]
 
 setup(
-    author="10 Academy",
-    email="train@10academy.org",
+    author="Yosef",
+    email="yosef@",
     python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -24,7 +24,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="A Repository structure for all your project",
+    description="TellCo Data Analysis",
     install_requires=requirements,
     long_description=readme,
     include_package_data=True,
@@ -33,7 +33,7 @@ setup(
     packages=find_packages(include=['scripts', 'scripts.*']),
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/10xac/PythonPackageStructure',
+    url='https://github.com/mozartofmath/10academyWeek1',
     version='0.1.0',
     zip_safe=False,
 )
